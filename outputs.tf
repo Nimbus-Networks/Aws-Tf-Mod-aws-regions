ea0d22db6350339a4ba1b170f2e2767e669869c9
Mitchell St Amant <m@mitchell.to>
2022-11-08 11:12:39 -0500

    remove paired region output

--- outputs.tf
+++ outputs.tf
@@ -17,23 +17,3 @@ output "location_slug" {
   value       = local.location_slug
   description = "Azure region in slug format"
 }
-
-output "paired_location" {
-  description = <<EOD
-Azure paired region with the current one. All formats available as attributes.
-```
-object({
-  location (string): Azure paired region in standard format
-  location_short (string): Azure paired region in short format for resource naming purpose
-  location_cli (string): Azure paired region in CLI name format
-  location_slug (string): Azure paired region in slug format
-})
-```
-EOD
-  value = local.paired_region == null ? null : {
-    location       = try(local.regions[local.paired_region], null)
-    location_short = try(local.short_names[local.paired_region], null)
-    location_cli   = try(local.cli_names[local.paired_region], null)
-    location_slug  = local.paired_region
-  }
-}
